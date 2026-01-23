719e57eacbdb819785d78b56d6f63e85fb3e71af
Marco Bahe <marcobahe@gmail.com>
2026-01-23 08:58:37 -0300

    Create popup-avisos.js

---
+++ popup-avisos.js
@@ -0,0 +1,13 @@
+<iframe
+  src="https://ghl-popup-pulse.lovable.app/popup-iframe"
+  style="display:none;width:100%;height:100%;border:none;border-radius:8px"
+  id="ghl-popup-1752074453394" 
+  data-layout="{'id':'POPUP'}"
+  data-trigger-type="subaccountOnly"
+  data-activation-type="immediate"
+  data-show-once="true"
+  data-debug="false"
+  title="GHL Popup Notification"
+>
+</iframe>
+<script src="https://ghl-popup-pulse.lovable.app/assets/popup-embed.js"></script>
